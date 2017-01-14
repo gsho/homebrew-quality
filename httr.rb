757ce26795782c4d395d0c79c5f79886cadf5320
Jun Sakata <jun_sk@volare.jp>
2017-01-14 21:03:01 +0900

    version 2

--- httr.rb
+++ httr.rb
@@ -1,11 +1,11 @@
 require "formula"
 
-HOMEBREW_HTTR_VERSION='0.0.1'
+HOMEBREW_HTTR_VERSION='0.0.2'
 
 class Httr < Formula
   url "https://github.com/sakajunquality/httr/releases/download/#{HOMEBREW_HTTR_VERSION}/httr-release-amd64.tar.gz"
   homepage "https://github.com/sakajunquality/httr"
-  sha256 "47d92f94eeecd686eb5e7a1a4a34161e8900a64965228935a25c42aed8c7313d"
+  sha256 "86a5a760876555748fc49af26b4368ee93ef6a54c847cd3aa7246244425aa342"
 
   version HOMEBREW_HTTR_VERSION
   head 'https://github.com/sakajunquality/httr.git', :branch => 'master'
